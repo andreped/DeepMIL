01a4e542a199f147cf82b67bea61e300182e2a50
Tor-Arne <tno033@uit.no>
2020-03-20 08:32:51 +0100

    almost done, problem with progbar

--- python/models.py
+++ python/models.py
@@ -2,12 +2,13 @@ from tensorflow.python.keras.layers import Input, Dense, Convolution2D, MaxPooli
     SpatialDropout2D, \
     ZeroPadding2D, Activation, AveragePooling2D, UpSampling2D, BatchNormalization, ConvLSTM2D, \
     TimeDistributed, Concatenate, Lambda, Reshape, UpSampling3D, Convolution3D, MaxPooling3D, \
-    SpatialDropout3D, multiply, GlobalAveragePooling2D, TimeDistributed, LSTM, Layer, GlobalMaxPooling2D
+    SpatialDropout3D, multiply, GlobalAveragePooling2D, TimeDistributed, LSTM, Layer, GlobalMaxPooling2D,add
 from tensorflow.python.keras.regularizers import l2
 from tensorflow.python.keras.models import Model, Sequential
 import tensorflow as tf
 from custom_layers import Mil_Attention, Last_Sigmoid
 from tensorflow.python.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras import regularizers
 
 
 def convolution_block_2d_fcn(x, nr_of_convolutions, use_bn=False, spatial_dropout=None, weight_decay=0, stride=1):
@@ -985,5 +986,64 @@ def model2D():  # TODO: Make this a class...
 
     out = Last_Sigmoid(output_dim=1, name='FC1_sigmoid')(x_mul)
     model = Model(inputs=[data_input], outputs=[out])
+
+    return model
+
+def residual_block(prev_layer, repetitions, num_filters):
+    block = prev_layer
+    for i in range(repetitions):
+        x = Convolution2D(
+                filters=num_filters,
+                kernel_size=3,
+                kernel_regularizer=regularizers.l2(1e-2),
+                bias_regularizer=regularizers.l2(1e-2),
+                strides=1,
+                padding='same'
+        )(block)
+        #x = Activation('relu')(x)
+        y = Activation('relu')(x)
+        #y = BatchNormalization()(x)
+        #y = BatchNormalization()(y)  # TODO: INTRODUCED BATCHNORM HERE -> resulted in high conf on class 1 only (float16 issue with batchnorm?)
+        x = Convolution2D(
+                filters=num_filters,
+                kernel_size=3,
+                #kernel_regularizer=regularizers.l2(1e-2),  # TODO: Tried to remove L2-regularization
+                #bias_regularizer=regularizers.l2(1e-2),  # TODO: Tried to remove L2-regularization
+                strides=1,
+                padding='same'
+        )(x)
+        x = add([x, y])
+        x = Activation('relu')(x)
+        #x = BatchNormalization()(x)
+        #x = BatchNormalization()(x)  # TODO: INTRODUCED BATCHNORM HERE
+        block = MaxPooling2D(pool_size=3, strides=2, padding='same')(x)
+
+    return block
+
+
+def resnet(input_shape, num_classes, ds=2):  # num_classes, num_channels=1, ds=2):
+
+    inputs = Input(shape=input_shape)  # (None, None, num_channels))
+
+    x = Convolution2D(
+        filters=64//ds,
+        kernel_size=7,
+        #kernel_regularizer=regularizers.l2(1e-2),  # TODO: Tried to remove L2-regularization
+        #bias_regularizer=regularizers.l2(1e-2),  # TODO: Tried to remove L2-regularization
+        strides=2,
+        padding='same'
+    )(inputs)
+    block_0 = MaxPooling2D(pool_size=3, strides=2, padding='same')(x)
+
+    block_1 = residual_block(prev_layer=block_0, repetitions=3, num_filters=64//ds)
+    block_2 = residual_block(prev_layer=block_1, repetitions=4, num_filters=128//ds)
+    block_3 = residual_block(prev_layer=block_2, repetitions=6, num_filters=256//ds)
+    block_4 = residual_block(prev_layer=block_3, repetitions=3, num_filters=512//ds)
+
+    x = GlobalAveragePooling2D()(block_4)
+    outputs = Dense(num_classes)(x)  # , activation="softmax")(x)  # TODO: Forgot sigmoid activation here in implementation??? No. softmax is applied in step_bag_gradient in train
+    #outputs = Dense(num_classes, dtype='float32')(x)  # TODO: Perhaps need to cast to float32 in last layer before softmax?
+
+    model = Model(inputs=inputs, outputs=outputs)
 
     return model
